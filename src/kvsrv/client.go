a00d061e327481e86521be799be12dd0262ec959
Julia Bier <juliabiersuriano@gmail.com>
2025-04-16 23:21:01 -0400

    implement basic client/server logic

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,9 +1,11 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
 
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
 	server *labrpc.ClientEnd
@@ -35,9 +37,14 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
+	args := GetArgs{
+		Key: key,
+	}
+	reply := GetReply{}
 
-	// You will have to modify this function.
-	return ""
+	ck.server.Call("KVServer.Get", &args, &reply)
+
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -49,8 +56,15 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
-	// You will have to modify this function.
-	return ""
+	args := PutAppendArgs{
+		Key:   key,
+		Value: value,
+	}
+	reply := PutAppendReply{}
+
+	ck.server.Call("KVServer."+op, &args, &reply)
+
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
